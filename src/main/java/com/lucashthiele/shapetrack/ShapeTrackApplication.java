f825abe4ec2190e86f518d9d052baa2fc91db4de
lucashthiele <lucasthiele0@outlook.com>
2023-11-08 19:55:18 -0300

    init

---
+++ src/main/java/com/lucashthiele/shapetrack/ShapeTrackApplication.java
@@ -0,0 +1,13 @@
+package com.lucashthiele.shapetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapeTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucashthiele/shapetrack/ShapeTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucashthiele.shapetrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapeTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
